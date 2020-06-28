d1aca35df69fad94cd4138f4aa3602caccf44f78
Tom Fleet <tomfleet2018@gmail.com>
2020-06-28 10:00:30 +0100

    Added additional context to setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,4 +1,13 @@
+from os import path
+
 from setuptools import find_packages, setup
+
+# Define project root folder for later refs
+ROOT = path.abspath(path.dirname(__file__))
+
+# Read in the README.md to form the long description
+with open(path.join(ROOT, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="src",
@@ -7,4 +16,6 @@ setup(
     description="{{cookiecutter.description}}",
     author="{{cookiecutter.author_name}}",
     license="{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
