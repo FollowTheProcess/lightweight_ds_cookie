126412b26997a108b50ca88105ce9c95b3339332
Tom Fleet <tomfleet2018@gmail.com>
2020-12-13 10:36:16 +0000

    Update with latest stuff from pypackage

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+# If GitHub cli used to create remote repo
+# This script will create the local & remote repos, link the two together, and make an initial commit
+
+{% if cookiecutter.use_gh_cli_to_create_repo == 'y' -%}
+
+# Already at the root of the generated project, no need to cd anywhere
+
+git init
+git add -A
+git commit -m "Initial Commit (Automated at Project Creation)"
+
+gh repo create {{cookiecutter.project_slug}} --confirm --description "{{cookiecutter.project_short_description}}"
+
+git push --set-upstream origin master
+
+{% endif %}
--- {{cookiecutter.project_slug}}/tasks.py
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -10,30 +10,11 @@ PROJECT_ROOT = Path(__file__).parent.resolve()
 
 
 @task
-def clean(c):
-    """
-    Recursively removes cache files and other clutter from the project.
-    """
-    c.run('find . -name "__pycache__" -exec rm -rf {} +')
-    c.run('find . -name ".pytest_cache" -exec rm -rf {} +')
-    c.run('find . -name ".mypy_cache" -exec rm -rf {} +')
-    c.run('find . -name ".ipynb_checkpoints" -exec rm -rf {} +')
-
-
-@task
 def style(c):
     """
     Runs style checking and linting tools on the project.
     """
     c.run("isort .")
-    c.run("flake8")
+    c.run("flake8 .")
     c.run("black .")
     c.run("mypy .")
-
-
-@task
-def test(c):
-    """
-    Runs the test suite.
-    """
-    c.run("pytest")
